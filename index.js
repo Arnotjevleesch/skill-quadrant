436bd0b4f4535f99b5bcc3d1f6d3959f4d8d0e2f
Arnotjevleesch <arnaud.ruant@gmail.com>
2018-09-28 15:40:00 +0200

    test KO

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const chart = new Vue({
     width: '1000', //to specify the width of the chart
     height: '600', //to specify the height of the chart
     dataFormat: 'json',
-    dataSource: myDataSource,
+    dataSource: Object.assign({}, myDataSource),
     boxesData:{}
   },
   async mounted () {
